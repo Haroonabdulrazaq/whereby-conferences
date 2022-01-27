356d835802996d88db45238ae6907f3351a64ae7
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-01-27 09:51:22 +0100

    Add interceptors for axios

--- src/components/Meeting.js
+++ src/components/Meeting.js
@@ -5,16 +5,18 @@ import './meeting.css';
 
 function Meeting() {
 
-  const API_KEY = process.env.REACT_APP_API_KEY;
+  // const API_KEY = process.env.REACT_APP_API_KEY;
   const createMeeting = async()=>{
-
     const response = await axios({
       method: 'GET',
+      headers: { Accept: 'application/json' },
       url: `${process.env.REACT_APP_BACKEND_URL}/`,
       params: {
         isLocked: true,
       },
     });
+    const result = await response;
+    console.log('I am the result of the API call', result.data);
   }
 
   return (
